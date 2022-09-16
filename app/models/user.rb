11e6e2713d2309825a8500a020ea7362691e972e
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-09-16 21:43:19 +0100

    Create user instance method for generating authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	before_create :generate_auth_token
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,4 +13,10 @@ class User < ApplicationRecord
 
   has_many :guest_reviews, class_name: "GuestReviews", foreign_key: "guest_id"
   has_many :host_reviews, class_name: "HostReviews", foreign_key: "host_id"
+
+	def generate_auth_token
+		begin
+			self.auth_token = Devise.friendly_token
+		end while self.class.exists?(auth_token: self.auth_token)
+	end
 end
