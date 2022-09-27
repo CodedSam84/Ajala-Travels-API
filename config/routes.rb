5438802e446b6f2319f2db737a78d9b94f4ca51e
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-09-27 07:38:17 +0100

    Added routes for all controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post '/new_user', to: 'registrations#create'
+      get '/confirmation', to: 'confirmations#show'
+      get '/send_confirmation', to: 'confirmations#resend_email'
+      post '/google', to: 'authentications#google'
+      post '/sign-in', to: 'authentications#sign_in'
+      get '/sign-out', to: 'authentications#sign_out'
+    end
+  end
 end
