08b300df226769310ccdd2dcf0e3abfafa5e5bed
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-09-28 19:02:31 +0100

    Fix bugs in Authentications Controller

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,28 +1,30 @@
 class Api::V1::AuthenticationsController < ApplicationController
   include HTTParty
+
   before_action :authenticate_with_token!, only: [:sign_out]
 
   def google
     if params[:access_token]
       url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params[:access_token]}"
       response = HTTParty.get(url)
-      parsed_response = JSON.parse(response)
+      user_data = response.parsed_response
 
-      user = User.find_by_email(parsed_response[:email])
-
+      user = User.find_by_email(user_data["email"])
+      
       if user
         user.generate_auth_token
         user.save
         render json: user, status: :ok
       else
-        user = User.new(
-        email: parsed_response[:email],
-        fullname: parsed_response[:name],
-        image: parsed_response[:picture],
-        uid: parsed_response[:kid],
+        @user = User.new(
+        email: user_data["email"],
+        password: Devise.friendly_token[0,20],
+        fullname: user_data["name"],
+        image: user_data["picture"],
+        uid: user_data["kid"],
         provider: "Google"
       )
-
+    
       render_response
       end
 
@@ -32,12 +34,16 @@ class Api::V1::AuthenticationsController < ApplicationController
   end
 
   def sign_in
-    user = User.find_by_email(params[:email])
+    begin
+      @user = User.find_by_email(params[:email])
 
-    if user&.user.valid_password?(params[:password])
-      render_response
-    else
-      render json: { error: "Invalid email or password", success: false }, status: :unauthorized
+      if @user && @user.valid_password?(params[:password])
+        render_response
+      else
+        render json: { error: "Wrong password", success: false }, status: :unauthorized
+      end
+    rescue
+      render json: { error: "No record found!", success: false }, status: :unprocessable_entity
     end
   end
 
@@ -51,12 +57,12 @@ class Api::V1::AuthenticationsController < ApplicationController
   private
 
   def render_response
-    user.generate_auth_token
+    @user.generate_auth_token
 
-    if user.save
-      render json: user, status: :ok
+    if @user.save
+      render json: @user, status: :ok
     else
-      render json: { error: user.errors.messages, success: false }, status: :unprocessable_entity
+      render json: { error: @user.errors.messages, success: false }, status: :unprocessable_entity
     end
   end
 end
