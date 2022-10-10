6f9b0db0ede935a002275c6f9dd52bdd801907de
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-10-10 16:30:14 +0100

    Add perform delivery configuration to development and production environment files to allow sending of confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,19 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'https://steady-sunshine-b851be.netlify.app/',
+    user_name:       Rails.application.credentials.dig(:google_username),
+    password:        Rails.application.credentials.dig(:google_password),
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 
+  }
+  
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
