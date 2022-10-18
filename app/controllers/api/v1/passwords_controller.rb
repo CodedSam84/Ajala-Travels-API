ab1d16419617023172deceab3e07f69e68514ea8
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-10-18 23:45:58 +0100

    Create custome passwords controller for handling password resets and update

---
+++ app/controllers/api/v1/passwords_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::PasswordsController < Devise::PasswordsController
+  def password_reset_instructions
+    self.resource = resource_class.send_reset_password_instructions(password_params)
+
+    if successfully_sent?(resource)
+      render json: { success: true }, status: :ok
+    else
+      render json: { error: "Something went wrong..." }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:email)
+  end
+end
