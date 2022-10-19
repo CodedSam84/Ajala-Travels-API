737f2195548cf821d5e7723254016e7d83db85ae
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-10-19 22:07:28 +0100

    Create update action for changing user password

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -10,12 +10,18 @@ class Api::V1::PasswordsController < Devise::PasswordsController
   end
 
   def update
-    
+    self.resource = resource_class.reset_password_by_token(password_params)
+
+    if resource.errors.empty?
+      render json: { success: true }, status: :ok
+    else
+      render json: { error: resource.errors }, status: :unprocessable_entity
+    end
   end
 
   private
 
   def password_params
-    params.permit(:email)
+    params.require(:user).permit(:email, :password, :password_confirmation, :reset_password_token)
   end
 end
